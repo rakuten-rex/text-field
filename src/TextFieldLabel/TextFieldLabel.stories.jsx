981674b06c261a8d7d504662ccffc05999bfa6cd
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2020-02-14 17:20:50 +0900

    Added - Theme props to TextFieldLabel component

--- src/TextFieldLabel/TextFieldLabel.stories.jsx
+++ src/TextFieldLabel/TextFieldLabel.stories.jsx
@@ -3,7 +3,9 @@
 /* eslint-disable import/extensions */
 import React from 'react';
 import TextFieldLabel from 'src/TextFieldLabel';
-import { withKnobs } from '../../.storybook/helper';
+import { cssVarsToLegacy, withKnobs } from '../../.storybook/helper';
+import TextFieldThemeProps from './themeProps';
+import LabelThemeProps from '../LabelUi/themeProps';
 
 /**
  * Main story
@@ -42,3 +44,107 @@ export const FocusAndActiveState = () => {
     />
   );
 };
+
+/**
+ * Custom Theme support
+ * */
+
+function Theme() {
+  const {
+    themeDefaultBorder,
+    themeDefaultPlaceholder,
+    themeDefaultText,
+    themeHoverBorder,
+    themeHoverPlaceholder,
+    themeHoverText,
+    themeActiveBorder,
+    themeActivePlaceholder,
+    themeActiveText,
+  } = TextFieldThemeProps();
+  const {
+    themeLabelDefaultText,
+    themeLabelHoverText,
+    themeLabelActiveText,
+  } = LabelThemeProps();
+
+  const customStyle = {
+    '--rex-text-field-theme-border': themeDefaultBorder,
+    '--rex-text-field-theme-placeholder': themeDefaultPlaceholder,
+    '--rex-text-field-theme-text': themeDefaultText,
+    '--rex-text-field-theme-hover-text': themeHoverText,
+    '--rex-text-field-theme-hover-border': themeHoverBorder,
+    '--rex-text-field-theme-hover-placeholder': themeHoverPlaceholder,
+    '--rex-text-field-theme-active-text': themeActiveText,
+    '--rex-text-field-theme-active-border': themeActiveBorder,
+    '--rex-text-field-theme-active-placeholder': themeActivePlaceholder,
+    '--rex-text-field-label-theme-text': themeLabelDefaultText,
+    '--rex-text-field-label-theme-hover-text': themeLabelHoverText,
+    '--rex-text-field-label-theme-active-text': themeLabelActiveText,
+  };
+
+  return {
+    customStyleHtml: cssVarsToLegacy(customStyle, TextFieldLabel),
+  };
+}
+
+export const WithThemeReactAndCSSVars = () => {
+  const {
+    themeDefaultBorder,
+    themeDefaultPlaceholder,
+    themeDefaultText,
+    themeHoverBorder,
+    themeHoverPlaceholder,
+    themeHoverText,
+    themeActiveBorder,
+    themeActivePlaceholder,
+    themeActiveText,
+  } = TextFieldThemeProps();
+  const {
+    themeLabelDefaultText,
+    themeLabelHoverText,
+    themeLabelActiveText,
+  } = LabelThemeProps();
+
+  return (
+    <TextFieldLabel
+      style={{
+        '--rex-text-field-theme-border': themeDefaultBorder,
+        '--rex-text-field-theme-placeholder': themeDefaultPlaceholder,
+        '--rex-text-field-theme-text': themeDefaultText,
+        '--rex-text-field-theme-hover-text': themeHoverText,
+        '--rex-text-field-theme-hover-border': themeHoverBorder,
+        '--rex-text-field-theme-hover-placeholder': themeHoverPlaceholder,
+        '--rex-text-field-theme-active-text': themeActiveText,
+        '--rex-text-field-theme-active-border': themeActiveBorder,
+        '--rex-text-field-theme-active-placeholder': themeActivePlaceholder,
+        '--rex-text-field-label-theme-text': themeLabelDefaultText,
+        '--rex-text-field-label-theme-hover-text': themeLabelHoverText,
+        '--rex-text-field-label-theme-active-text': themeLabelActiveText,
+      }}
+      id="age"
+      name="firstname"
+      placeholder="Placeholder"
+      htmlFor="firstname"
+      label="Label"
+      labelId="labelId"
+    />
+  );
+};
+
+export const WithThemeHTMLAndLegacyCSS = () => {
+  const { customStyleHtml } = Theme();
+
+  return (
+    <>
+      <style>{customStyleHtml}</style>
+      <TextFieldLabel
+        id="age"
+        name="firstname"
+        placeholder="Placeholder"
+        htmlFor="firstname"
+        label="Label"
+        labelId="labelId"
+      />
+    </>
+  );
+}
