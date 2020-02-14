df457c68755b96874802e8834cf4f2458eb9b38c
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2020-02-14 13:51:53 +0900

    Added - Docs for github pages and README file

--- docs/TextFieldUi/TextFieldUi.development.js
+++ docs/TextFieldUi/TextFieldUi.development.js
@@ -1,6 +1,6 @@
 /*!
  * 
- * @license @rakuten-rex/text-field v1.1.0 2020-02-14
+ * @license @rakuten-rex/text-field v1.1.1 2020-02-14
  * TextFieldUi/TextFieldUi.development.js
  * 
  * Copyright (c) 2018-present, Rakuten, Inc.
